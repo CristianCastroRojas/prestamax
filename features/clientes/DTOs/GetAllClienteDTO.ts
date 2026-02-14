ba870151f3235014e289a9667e6703d64bef5e9c
Cristian Andres Castro Rojas <cristiancastro2406@gmail.com>
2026-02-14 15:11:18 -0500

    feat(clientes): ampliar GetAllClienteDTO con datos adicionales del cliente

--- features/clientes/DTOs/GetAllClienteDTO.ts
+++ features/clientes/DTOs/GetAllClienteDTO.ts
@@ -3,7 +3,16 @@ export interface GetAllClienteDTO {
   nombre: string;
   apellido: string;
   numero_documento: string;
+  correo: string;
+  fecha_nacimiento: string;
+
+  telefono: string;
+  barrio: string;
+  direccion: string;
   estado: boolean;
+  id_tipo_documento: number;
+  id_ciudad: number;
+  id_departamento: number;
   // Estos campos vienen de los JOINs que hicimos en la consulta
   tipoDocumento: string; 
   ciudad: string;
